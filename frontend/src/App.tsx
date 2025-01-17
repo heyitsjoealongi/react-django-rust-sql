28d14445888ed44f91d66a52ae3cfb8931409f2b
Joe Alongi <email@heyitsjoealongi.com>
2025-01-16 21:03:38 -0500

    React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <p>ship.bitsbythebyte.pub</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
