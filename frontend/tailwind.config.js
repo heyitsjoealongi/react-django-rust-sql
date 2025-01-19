ba6a248f24b65b49de5ea7396a1c1b89863c0907
Joe Alongi <email@heyitsjoealongi.com>
2025-01-19 16:51:15 -0500

    Branding

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: "#020617",
-        darkoff: "#05103F",
-        light: "#2563EB",
+        dark: "#170202",
+        darkoff: "#3F0505",
+        light: "#EB2525",
         bright: "#FFFFFF",
         accent: "#FFAD05",
         shade: "#8A7E72",
