4b8b4dce57ca9bd01b0282aefffddc93176afe87
Joe Alongi <email@heyitsjoealongi.com>
2025-01-24 21:54:41 -0500

    Links & Static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Uncomment if you have extra static files and a directory in your GitHub repo.
 # If you don't have this directory and have this uncommented your build will fail
